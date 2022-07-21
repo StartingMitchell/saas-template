c29647b907da1b32da6be75662ae0aef455d5a49
StartingMitchell <mitchell@startingsecurity.com>
2022-07-20 19:48:19 -0700

    init

--- front-end/deployment/config/config-development.ts
+++ front-end/deployment/config/config-development.ts
@@ -14,10 +14,10 @@ const configDevelopment = {
   /**
    * Add the details from the Pulumi output here, after running 'pulumi up'
    */
-  USER_POOL_CLIENT_ID: '',
-  USER_POOL_ID: '',
-  IDENTITY_POOL_ID: '',
-  GRAPHQL_ENDPOINT: '',
+  USER_POOL_CLIENT_ID: '6s9itta957qbqd7clgg1k11ntr',
+  USER_POOL_ID: 'us-west-1_hJ9Ij8WGJ',
+  IDENTITY_POOL_ID: 'us-west-1:36e6e327-abe2-4b7e-b2a7-a716ea74890d',
+  GRAPHQL_ENDPOINT: 'https://jgltnj4jfjcjnn4q7kfziz2ybq.appsync-api.us-west-1.amazonaws.com/graphql',
 };
 
 export default configDevelopment;
